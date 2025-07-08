6e24beef104674563fa2c8c88ac0e6128be7e3fd
LexRilenko <kurilenko16573@gmail.com>
2025-07-08 16:26:55 +0300

    gloss support added

--- algorithms-defence/app/Main.hs
+++ algorithms-defence/app/Main.hs
@@ -2,8 +2,8 @@ module Main where
 
 import Graphics.Gloss
 
-main :: IO ()
-main = display
-         (InWindow "Gloss Window" (800, 600) (100, 100))
+main :: IO (lay
+         (I)
+main = dispnWindow "Gloss Window" (800, 600) (100, 100))
          white
          (Circle 100)
